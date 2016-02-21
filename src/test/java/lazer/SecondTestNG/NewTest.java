de240af1a6cf984884c6c435516eeea67b4a28b7
Lazer Morris <lazermorris@gmail.com>
2016-02-21 13:04:56 -0500

    Feb 21 First Commit

--- src/test/java/lazer/SecondTestNG/NewTest.java
+++ src/test/java/lazer/SecondTestNG/NewTest.java
@@ -1,14 +1,18 @@
 package lazer.SecondTestNG;
 
 import org.testng.annotations.Test;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.SendKeysAction;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -26,6 +30,7 @@ public class NewTest {
 	  System.setProperty("webdriver.chrome.driver",path);
 	  WebDriver driver = new ChromeDriver();
 	  JavascriptExecutor js = (JavascriptExecutor)driver;
+	  Actions action = new Actions(driver);
 	  driver.manage().window().maximize();
 	  driver.navigate().to("https://travel.americanexpress.com/home");
 	  WebDriverWait wait = new WebDriverWait(driver, 30);
@@ -54,6 +59,22 @@ public class NewTest {
 	  //Assert.assertTrue(b.startsWith(""));;
 	  //Assert.assertTrue(b.endsWith(""));;
 	  System.out.println("Success");
+	  action.moveToElement(driver.findElement(By.cssSelector("a[data-target='#flightHotel'][id ='hpFlightHotelTab']"))).click().perform();
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  //action.keyDown(Keys.CONTROL).sendKeys(Keys.F5).perform();
+	  
+	  js.executeScript("window.scrollBy(0,-2500)", "");
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  driver.findElement(By.xpath("//div/div/div/ul/li/a[@href='https://travel.americanexpress.com/travel-offers?inav=travel_offers2_gem']"));
+	  //action.moveToElement(driver.findElement(By.cssSelector("a[title id='travel_offers2_gem']")));
+	 // action.click();
+	
+	 action.moveToElement(driver.findElement(By.xpath("//div/div/div/ul/li/a[@href='https://travel.americanexpress.com/travel-offers?inav=travel_offers2_gem']"))).click();
+	  //action.perform();
+	  
+	 // action.moveToElement(driver.findElement(By.xpath("//div/span/input[@class='signupEmailField ng-pristine ng-invalid ng-invalid-email']"))).perform();
+	  
+	  
 	  driver.quit();
   }
   @BeforeMethod
